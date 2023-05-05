aeaa6b0bcabbef1fc0012a29d181a86330f86e5a
newkincode <maainnewkin59@gmail.com>
2023-05-05 18:15:47 +0900

    프래임이 바껴도 일정하게 움직이게 변경

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
 BACKGROUND = pygame.image.load("img/surface_background.png")
 
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_HEIGHT))
 
 # pygame display setup
@@ -24,20 +25,23 @@ CHARACTER_POS: pygame.Vector2 = pygame.Vector2(CHARACTER_WIGHT / 2, CHARACTER_HE
 is_runing = True
 
 while is_runing:
+    df = clock.tick(100) / 1000
+    clock.tick(100)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_runing = False
-            
+
         if event.type == pygame.KEYDOWN:
             match event.key:
                 case pygame.K_UP: # up 키를 눌렀을때
-                    CHARACTER_POS.y -= 10 # 캐릭터의 y 좌표를 10만큼 빼준다
+                    CHARACTER_POS.y -= df+10 # 캐릭터의 y 좌표를 10만큼 빼준다
                 case pygame.K_DOWN: # down 키를 눌렀을때
-                    CHARACTER_POS.y += 10 # 캐릭터의 y 좌표를 10만큼 더해준다
+                    CHARACTER_POS.y += df+10 # 캐릭터의 y 좌표를 10만큼 더해준다
                 case pygame.K_LEFT: # left 키를 눌렀을때
-                    CHARACTER_POS.x -= 10 # 캐릭터의 x 좌표를 10만큼 빼준다
+                    CHARACTER_POS.x -= df+10 # 캐릭터의 x 좌표를 10만큼 빼준다
                 case pygame.K_RIGHT: # right 키를 눌렀을때
-                    CHARACTER_POS.x += 10 # 캐릭터의 x 좌표를 10만큼 더해준다
+                    CHARACTER_POS.x += df+10 # 캐릭터의 x 좌표를 10만큼 더해준다
                 
 
     screen.blit(BACKGROUND, (0, 0))
